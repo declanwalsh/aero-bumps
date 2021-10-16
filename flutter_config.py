d3064c4663c42f39109d36b5dfe68908d58e80c0
Declan Walsh <dek.other@pm.me>
2021-10-16 23:29:09 +1100

    Reformat flutter_config

--- flutter_config.py
+++ flutter_config.py
@@ -1,34 +1,39 @@
 # -*- coding: utf-8 -*-
-"""
-General (non-analysis specific) configuration files
-@author: dek_work
+"""General (non-analysis specific) configuration files
+
+Split into two separate sections:
+- This file (cfg) for general program configurations
+- Iimported file (cfg_analysis) for analysis specific configurations (including columns, header rows, etc.).
+
+    - import flutter_config as cfg
+    - from flutter_config import cfg_analysis
 """
 
-# !!!!!!!!!!!!!!!!!!!!!!!!
+# -----------------
 # load configuration file in the /config directory here
-# !!!!!!!!!!!!!!!!!!!!!!!!
+# ------------------
 from config import config_DAQ11270_000009 as cfg_analysis
 
-DEBUG = False # shows debugging data for program printed in console (not vibration data)
-SHOW_DETAIL = True # shows additional vibration data
+DEBUG = False  # shows debugging data for program printed in console (not vibration data)
+SHOW_DETAIL = True  # shows additional vibration data
 
-FILTER_DAMPING = True # filters data 
-CALC_DAMPING = True # calculates damping
-CALC_FREQ = True # calculate peak frequencies from FFT
+FILTER_DAMPING = True  # filters data
+CALC_DAMPING = False  # calculates damping
+CALC_FREQ = True  # calculate peak frequencies from FFT
 
-PLOT_FFT = True # plots the FFT
-PLOT_DATA = True # plots the actual data
+PLOT_FFT = True  # plots the FFT
+PLOT_DATA = True  # plots the actual data
 
-CHECK_STAT = False # checks some statistical measures on data (stationary)
+CHECK_STAT = False  # checks some statistical measures on data (stationary)
 
-SAVE_FIG = True # saves all plotted figures to png in the working directory
-SAVE_OUTPUT = True # saves frequencies in a csv
+SAVE_FIG = True  # saves all plotted figures to png in the working directory
+SAVE_OUTPUT = True  # saves frequencies in a csv
 
 # Folder relative to program
 # TODO - automatically generate folders if they are not already present in the directory
-CSV_FILE_ROOT = "Data/" # input CSV's
-IMAGE_FILE_ROOT = "Images/" # output images
-OUTPUT_FILE_ROOT = "Results/" # output csv summaries
+CSV_FILE_ROOT = "Data/"  # input CSV's
+IMAGE_FILE_ROOT = "Images/"  # output images
+OUTPUT_FILE_ROOT = "Results/"  # output csv summaries
 FILTERED_IMAGE_FILE_ROOT = "Filtered/"
 
 # matplotlib figure sizes in inches
@@ -39,12 +44,15 @@ LIMITS = [-1.5, 3]
 # standard high order for Butterworth filters
 FILTER_ORDER = 4
 
-# columns in numpy array for storing data in program memory
+# columns in numpy array for storing vibration data in program memory
 COL_IDX = 0
 COL_TIME = 1
 COL_SIGNAL = 2
 COL_FILTERED = 3
 
+# columns in numpy array for storing atmospheric data in program memory
+# maintains COL_IDX and COL_TIME as before
+# different sample rate to vibration data requires different arrays
 COL_PRESSURE = 2
 COL_TEMP = 3
 COL_ALT = 4
